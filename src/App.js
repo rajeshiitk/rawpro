116cd10b7a5f43b914be928ceec8bdf629e28f1b
Rajesh Choudhary <rajuvert18@gmail.com>
2023-02-27 01:33:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import './styles/App.scss';
+import './styles/Header.scss';
+import './styles/Home.scss';
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
